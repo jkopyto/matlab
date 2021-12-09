77c9bd1a2df6d817dd339c97e165360a70045720
Jakub Kopyto <50829078+jkopyto@users.noreply.github.com>
2021-12-09 19:37:23 +0100

    update

--- badanie.m
+++ badanie.m
@@ -1,6 +1,6 @@
 function x = badanie(A)
   clc
-  disp("Badana macierz ="), disp(A);
+  #disp("Badana macierz ="), disp(A);
   
   [Q, R] = qr(A);
   disp("\nWspolczynnik uwarunkowania macierzy = "), disp(cond(A));
@@ -20,7 +20,7 @@ function x = badanie(A)
   x_gauss = gauss(A, b);
   
   disp("\nCzas obliczen dla metody gaussa ="), disp(etime(clock(), time_gauss));
-  disp("\nx = "), disp(x_gauss);
+  #disp("\nx = "), disp(x_gauss);
   
   #QR
   time_qr = clock();
@@ -28,7 +28,7 @@ function x = badanie(A)
   x_qr = R \ Q' * b;
   
   disp("\nCzas obliczen przy uzyciu rozkladu QR ="), disp(etime(clock(), time_qr));
-  disp("\nx = "), disp(x_qr);
+  #disp("\nx = "), disp(x_qr);
   
   accurate_sol = A \ b;
   
